67fa70d8105ea3298b5fab184e8c21cb464e20c6
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-08-24 15:33:21 +0800

    Update

---
+++ data/Day1/CS_Solution/Ap06/Program.cs
@@ -0,0 +1,10 @@
+﻿int a;
+short b;
+
+//b = 10000;
+//a = b;
+
+a = 1000000;
+b = (short)a; 
+
+Console.WriteLine($"a={a}({a:x8}), b={b}({b:x4})");
---
+++ data/Day1/CS_Solution/Ap07/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("byte:size={0}, {1} ~ {2}", 
+    sizeof(byte), byte.MinValue, byte.MaxValue);
+
+Console.WriteLine("short:size={0}, {1} ~ {2}",
+    sizeof(short), short.MinValue, short.MaxValue);
+
+Console.WriteLine("int:size={0}, {1} ~ {2}",
+    sizeof(int), int.MinValue, int.MaxValue);
+
+Console.WriteLine("long:size={0}, {1} ~ {2}",
+    sizeof(long), long.MinValue, long.MaxValue);
+
+Console.WriteLine("float:size={0}, {1} ~ {2}",
+    sizeof(float), float.MinValue, float.MaxValue);
+
+Console.WriteLine("double:size={0}, {1} ~ {2}",
+    sizeof(double), double.MinValue, double.MaxValue);
+
+Console.WriteLine("decimal:size={0}, {1} ~ {2}",
+    sizeof(decimal), decimal.MinValue, decimal.MaxValue);
+
+decimal x = 10L+ 21.5M;
