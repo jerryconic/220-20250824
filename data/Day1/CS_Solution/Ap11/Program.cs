d685427502f9f66675213a6e0b442a09efd649e0
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-08-24 16:07:37 +0800

    Update

---
+++ data/Day1/CS_Solution/Ap11/Program.cs
@@ -0,0 +1,19 @@
+﻿string s;
+int income, tax;
+
+Console.Write("年所得收入:");
+s = Console.ReadLine();
+income = int.Parse(s);
+
+if (income <= 540000)
+    tax = (int)(income * 0.05 + 0.5);
+else if (income <= 1210000)
+    tax = (int)(income * 0.12 - 37800 + 0.5);
+else if (income <= 2420000)
+    tax = (int)(income * 0.2 - 134600 + 0.5);
+else if (income <= 4530000)
+    tax = (int)(income * 0.3 - 376600 + 0.5);
+else
+    tax = (int)(income * 0.4 - 829600 + 0.5);
+
+Console.WriteLine($"年所得收入:{income:#,##0}, 應繳所得稅:{tax:#,##0}");
