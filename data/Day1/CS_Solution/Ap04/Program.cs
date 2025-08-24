e8e11c0a5dc880d0622c9ce6fdaedd7c159feab8
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-08-24 13:53:16 +0800

    Update

---
+++ data/Day1/CS_Solution/Ap04/Program.cs
@@ -0,0 +1,6 @@
+﻿string s;
+Console.Write("Your name:");
+s = Console.ReadLine();
+//Console.WriteLine("Hello, " + s + "!");
+//Console.WriteLine("Hello, {0}!", s); //Format String(格式化字串)
+Console.WriteLine($"Hello, {s}!");  //字串插補(Interpolation)
---
+++ data/Day1/CS_Solution/Ap05/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+int a, b;
+a = 10;
+b = 7;
+Console.WriteLine($"{a} + {b} = {a + b}");
+Console.WriteLine($"{a} - {b} = {a - b}");
+Console.WriteLine($"{a} * {b} = {a * b}");
+Console.WriteLine($"{a} / {b} = {a / b}");
+Console.WriteLine($"{a} % {b} = {a % b}");
+*/
+//float a, b;
+//double a, b;
+decimal a, b;
+a = 10;
+b = 7;
+Console.WriteLine($"{a} / {b} = {a / b}");
