d189954de271fbe80bb59e18d22b588adb916e56
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-08-24 18:28:15 +0800

    Interest

---
+++ data/Day1/CS_Solution/Ap15/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+for(float i = 0; i<=10; i+=0.1f)
+    Console.WriteLine(i);
+*/
+/*
+for (double i = 0; i < 10; i += 0.1)
+    Console.WriteLine(i);
+*/
+/*
+for (decimal i = 0; i <= 10; i += 0.1m)
+    Console.WriteLine(i);
+*/
+
+for (int i = 0; i < 100; i ++)
+    Console.WriteLine(i*0.1m);
+
