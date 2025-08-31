74d62e131c71ca53429061c030d13793ee983a0c
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-08-31 18:22:52 +0800

    ProductCRUD

---
+++ data/Day3/ProductCRUD/Form1.cs
@@ -0,0 +1,70 @@
+using Microsoft.Data.SqlClient;
+
+namespace ProductCRUD
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "Server=.;Database=db01;Trusted_Connection=true;TrustServerCertificate=True;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(connstr);
+            cn.Open();
+            cn.Close();
+            MessageBox.Show("Connect OK");
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(connstr);
+            SqlCommand cmd = cn.CreateCommand();
+            cmd.CommandText = @"INSERT INTO dbo.Product(prdt_id, prdt_name, price)
+                                VALUES(@id, @name, @price)";
+            cmd.Parameters.AddWithValue("@id", txtId.Text);
+            cmd.Parameters.AddWithValue("@name", txtName.Text);
+            cmd.Parameters.AddWithValue("@price", txtPrice.Text);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Insert OK");
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(connstr);
+            SqlCommand cmd = cn.CreateCommand();
+            cmd.CommandText = @"UPDATE dbo.Product
+                                SET prdt_name = @name, price = @price
+                                WHERE prdt_id = @id";
+
+            cmd.Parameters.AddWithValue("@id", txtId.Text);
+            cmd.Parameters.AddWithValue("@name", txtName.Text);
+            cmd.Parameters.AddWithValue("@price", txtPrice.Text);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Update OK");
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlConnection cn = new SqlConnection(connstr);
+            SqlCommand cmd = cn.CreateCommand();
+            cmd.CommandText = @"DELETE FROM dbo.Product
+                                WHERE prdt_id = @id";
+
+            cmd.Parameters.AddWithValue("@id", txtId.Text);
+
+            cn.Open();
+            cmd.ExecuteNonQuery();
+            cn.Close();
+            MessageBox.Show("Delete OK");
+        }
+    }
+}
