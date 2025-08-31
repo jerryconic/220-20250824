e0c27ba4bc962b6b8c4f5dc5eb003e82027cda0a
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-08-31 11:26:26 +0800

    GuessGame-Complete

---
+++ data/Day3/CS_Solution/Ap07/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Ap07
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnQuit = new Button();
+            btnGuess = new Button();
+            btnStart = new Button();
+            grpGuess = new GroupBox();
+            txtGuess = new TextBox();
+            label1 = new Label();
+            grpGuess.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnQuit
+            // 
+            btnQuit.Location = new Point(223, 152);
+            btnQuit.Name = "btnQuit";
+            btnQuit.Size = new Size(69, 34);
+            btnQuit.TabIndex = 7;
+            btnQuit.Text = "離開";
+            btnQuit.UseVisualStyleBackColor = true;
+            btnQuit.Click += btnQuit_Click;
+            // 
+            // btnGuess
+            // 
+            btnGuess.Enabled = false;
+            btnGuess.Location = new Point(148, 152);
+            btnGuess.Name = "btnGuess";
+            btnGuess.Size = new Size(69, 34);
+            btnGuess.TabIndex = 6;
+            btnGuess.Text = "猜數字";
+            btnGuess.UseVisualStyleBackColor = true;
+            btnGuess.Click += btnGuess_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(73, 152);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(69, 34);
+            btnStart.TabIndex = 5;
+            btnStart.Text = "開始";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // grpGuess
+            // 
+            grpGuess.Controls.Add(txtGuess);
+            grpGuess.Controls.Add(label1);
+            grpGuess.Enabled = false;
+            grpGuess.Location = new Point(56, 37);
+            grpGuess.Name = "grpGuess";
+            grpGuess.Size = new Size(236, 95);
+            grpGuess.TabIndex = 4;
+            grpGuess.TabStop = false;
+            grpGuess.Text = "猜數字";
+            // 
+            // txtGuess
+            // 
+            txtGuess.Location = new Point(77, 35);
+            txtGuess.Name = "txtGuess";
+            txtGuess.Size = new Size(138, 28);
+            txtGuess.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 43);
+            label1.Name = "label1";
+            label1.Size = new Size(43, 20);
+            label1.TabIndex = 0;
+            label1.Text = "1-99";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(373, 222);
+            Controls.Add(btnQuit);
+            Controls.Add(btnGuess);
+            Controls.Add(btnStart);
+            Controls.Add(grpGuess);
+            Font = new Font("Microsoft JhengHei UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 136);
+            Margin = new Padding(4);
+            Name = "Form1";
+            Text = "Form1";
+            grpGuess.ResumeLayout(false);
+            grpGuess.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnQuit;
+        private Button btnGuess;
+        private Button btnStart;
+        private GroupBox grpGuess;
+        private TextBox txtGuess;
+        private Label label1;
+    }
+}
---
+++ data/Day3/CS_Solution/Ap07/Form1.cs
@@ -0,0 +1,45 @@
+namespace Ap07
+{
+    public partial class Form1 : Form
+    {
+        int ans, guess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            ans = rnd.Next(1, 100);
+            Text = ans.ToString();
+            grpGuess.Enabled = true;
+            btnStart.Enabled = false;
+            btnGuess.Enabled = true;
+            txtGuess.Clear();
+            txtGuess.Select();
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            int.TryParse(txtGuess.Text, out guess);
+            if (guess < ans)
+                MessageBox.Show("高一點");
+            else if (guess > ans)
+                MessageBox.Show("低一點");
+            else
+            {
+                MessageBox.Show("猜對了");
+                grpGuess.Enabled = false;
+                btnStart.Enabled = true;
+                btnGuess.Enabled = false;
+            }
+        }
+
+        private void btnQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
