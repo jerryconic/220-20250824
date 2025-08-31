7c491b16bf5f16372522de5c57f3df20432cf891
Jerry Huang <85143679+jerryconic@users.noreply.github.com>
2025-08-31 17:25:53 +0800

    SQL

--- data/Day3/SQL/02_SELECT_Employee.sql
+++ data/Day3/SQL/02_SELECT_Employee.sql
@@ -14,3 +14,33 @@ WHERE birth_date > '1991-1-1';
 
 SELECT emp_id, emp_name FROM dbo.Employee
 WHERE emp_name <> 'John';
+
+SELECT * FROM dbo.Employee
+WHERE emp_id IN (1, 3, 5);
+
+SELECT * FROM dbo.Employee
+WHERE salary BETWEEN 30000 AND 40000;
+
+SELECT * FROM dbo.Employee
+WHERE emp_name LIKE 'A%';
+
+SELECT * FROM dbo.Employee
+WHERE emp_name LIKE '%A%';
+
+SELECT * FROM dbo.Employee
+WHERE emp_name LIKE '%A';
+
+SELECT * FROM dbo.Employee
+WHERE emp_name LIKE 'J___';
+
+SELECT * FROM dbo.Employee
+WHERE emp_name LIKE 'J____';
+
+UPDATE dbo.Employee
+SET emp_name = 'xxx', salary = 0
+WHERE emp_id = 3;
+
+DELETE FROM dbo.Employee
+WHERE emp_id = 4;
+
+SELECT * FROM dbo.Employee;
